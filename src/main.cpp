fb5e9ce3d0fbdd8bed2358c8f16b6134af1a9609
Imaniac230 <horelican.t@gmail.com>
2019-02-16 15:12:32 +0100

    separated into headder and main files

---
+++ src/main.cpp
@@ -0,0 +1,88 @@
+#include "urlsearch.h"
+
+
+int main()
+	{
+	std::cout << choose_url;
+
+	size_t count_urls = 0, count_searches = 0, nof_empty_searches = 0, desired_line = 1;
+	std::ofstream ofile;
+	std::string url;
+	std::cin >> url;
+	std::string url_ref = url;
+
+	std::cout << std::endl << start_url_message << url << std::endl << std::endl;
+
+	while(1)
+		{
+		if(DownloadFile(url, stored_url_contents))
+			for(std::string hlink : ExtractHyperlinks(stored_url_contents))
+				{
+				if(!hlink.compare(0, 4, "http") /*&& (hlink.find(' ') == std::string::npos)*/ && FindString(hlink, stored_urls))
+					{
+					ofile.open(stored_urls, std::ios::app);
+
+#ifdef OPEN_PAGES_IN_BROWSER					
+					BSTR b_hlink = _com_util::ConvertStringToBSTR(hlink.c_str());
+					LPCWSTR lp_hlink = b_hlink;
+					ShellExecute(NULL, L"open", BROWSER, lp_hlink, NULL, SW_SHOWNORMAL);
+					SysFreeString(b_hlink);
+#endif /* OPEN_PAGES_IN_BROWSER */
+
+#ifdef SKIP_REPEATING_DOMAINS
+					if((url.find(SubString(2, 3, '/', hlink)) == std::string::npos) && (hlink.find("accounts.google.com") == std::string::npos))
+#endif /* SKIP_REPEATING_DOMAINS */
+						++count_urls;
+
+					std::cout << hlink << std::endl;
+					url = hlink;
+					ofile << hlink << std::endl;
+					ofile.close();
+					break;
+					}
+				}
+
+		++count_searches;
+		if((count_searches - count_urls) > LIMIT_OF_EMPTY_SEARCHES_LOCAL)
+			{
+			if(count_urls == 0)
+				++nof_empty_searches;
+			url = url_ref;
+			count_urls = count_searches = 0;
+			}
+
+		if(nof_empty_searches > LIMIT_OF_EMPTY_SEARCHES_GLOBAL)
+			{
+			std::cout << std::endl << LIMIT_OF_EMPTY_SEARCHES_GLOBAL << empty_searches_message;
+
+			size_t current_line = 1;
+			std::ifstream ifl;
+			std::string newpage;
+
+			ifl.open(stored_urls);
+			while((current_line <= desired_line) && (getline(ifl, newpage)))
+				{ ++current_line; }
+			ifl.close();
+
+			++desired_line;
+			if((current_line) != desired_line)
+				{
+				std::cout << std::endl << choose_new_url;
+
+				while(!_kbhit())
+					{ Beep(500, 500); }
+
+				desired_line = current_line;
+				std::cin >> newpage;
+				}
+
+			url_ref = url = newpage;
+			nof_empty_searches = count_urls = count_searches = 0;
+
+			std::cout << std::endl << new_url_message << url << std::endl << std::endl;
+			}
+		}
+
+	ofile.close();
+	return 0;
+	}
--- src/urlsearch.h
+++ src/urlsearch.h
@@ -1,3 +1,6 @@
+#ifndef __URLSEARCH_H__
+#define __URLSEARCH_H__
+
 #include <cstdio>
 #include <cstring>
 #include <sstream>
@@ -15,7 +18,7 @@
 #pragma comment( lib, "urlmon" )
 
 //#define OPEN_PAGES_IN_BROWSER
-//#define SKIP_REPEATING_DOMAINS
+#define SKIP_REPEATING_DOMAINS
 
 #define LIMIT_OF_EMPTY_SEARCHES_LOCAL 3
 #define LIMIT_OF_EMPTY_SEARCHES_GLOBAL 3
@@ -28,14 +31,16 @@ constexpr auto empty_searches_message = "\a searches complete without changes.";
 constexpr auto choose_new_url = "Exhausted all possibilities for initial starting url, choose another starting url: ";
 constexpr auto new_url_message = "Using new url: ";
 
-
 #ifdef OPEN_PAGES_IN_BROWSER
 constexpr auto BROWSER = L"firefox";
 #pragma comment(lib, "comsuppw.lib")
 #pragma comment(lib, "kernel32.lib")
 #endif /* OPEN_PAGES_IN_BROWSER */
 
-
+enum TError
+	{
+	EBadParameters = 0,
+	};
 
 bool DownloadFile(const std::string aUrl, const std::string aPath)
 	{
@@ -60,6 +65,9 @@ std::set<std::string> ExtractHyperlinks(const std::string aFileName)
 
 size_t FindString(const std::string aStr, const char* aFileName)
 	{
+	if(!aFileName)
+		throw EBadParameters;
+
 	std::ifstream ifile;
 	std::string linestr;
 	ifile.open(aFileName);
@@ -79,9 +87,8 @@ size_t FindString(const std::string aStr, const char* aFileName)
 
 std::string SubString(const size_t aStart, const size_t aEnd, const char aChar, std::string aStr)
 	{
-
 	if(aStart == aEnd)
-		throw 1;
+		throw EBadParameters;
 
 	bool start = false, end = false;
 	size_t cur_pos = 0, occur = 0, start_pos = 0, end_pos = 0;
@@ -103,92 +110,9 @@ std::string SubString(const size_t aStart, const size_t aEnd, const char aChar, 
 		}
 
 	if(start_pos == 0 && end_pos == 0)
-		throw 1;
+		return "";
 
 	return aStr.substr(start_pos + 1, end_pos - start_pos - 1);
 	}
 
-
-int main()
-	{
-	std::cout << choose_url;
-
-	size_t count_urls = 0, count_searches = 0, nof_empty_searches = 0, desired_line = 1;
-	std::ofstream ofile;
-	std::string url;
-	std::cin >> url;
-	std::string url_ref = url;
-
-	std::cout << std::endl << start_url_message << url << std::endl << std::endl;
-
-	while(1)
-		{
-		if(DownloadFile(url, stored_url_contents))
-			for(std::string hlink : ExtractHyperlinks(stored_url_contents))
-				{
-				if(!hlink.compare(0, 4, "http") /*&& (hlink.find(' ') == std::string::npos)*/ && FindString(hlink, stored_urls))
-					{
-					ofile.open(stored_urls, std::ios::app);
-
-#ifdef OPEN_PAGES_IN_BROWSER					
-					BSTR b_hlink = _com_util::ConvertStringToBSTR(hlink.c_str());
-					LPCWSTR lp_hlink = b_hlink;
-					ShellExecute(NULL, L"open", BROWSER, lp_hlink, NULL, SW_SHOWNORMAL);
-					SysFreeString(b_hlink);
-#endif /* OPEN_PAGES_IN_BROWSER */
-
-					if((url.find(SubString(2, 3, '/', hlink)) == std::string::npos) && (hlink.find("accounts.google.com") == std::string::npos))
-						++count_urls;
-
-					std::cout << hlink << std::endl;
-					url = hlink;
-					ofile << hlink << std::endl;
-					ofile.close();
-					break;
-					}
-				}
-		++count_searches;
-		//cout << "One search complete." << endl;
-		if((count_searches - count_urls) > LIMIT_OF_EMPTY_SEARCHES_LOCAL)
-			{
-			if(count_urls == 0)
-				++nof_empty_searches;
-			url = url_ref;
-			count_urls = count_searches = 0;
-			}
-
-		if(nof_empty_searches > LIMIT_OF_EMPTY_SEARCHES_GLOBAL)
-			{
-			std::cout << std::endl << LIMIT_OF_EMPTY_SEARCHES_GLOBAL << empty_searches_message;
-
-			size_t current_line = 1;
-			std::ifstream ifl;
-			std::string newpage;
-
-			ifl.open(stored_urls);
-			while((current_line <= desired_line) && (getline(ifl, newpage)))
-				{ ++current_line; }
-			ifl.close();
-
-			++desired_line;
-			if((current_line) != desired_line)
-				{
-				std::cout << std::endl << choose_new_url;
-
-				while(!_kbhit())
-					{ Beep(500, 500); }
-
-				desired_line = current_line;
-				std::cin >> newpage;
-				}
-
-			url_ref = url = newpage;
-			nof_empty_searches = count_urls = count_searches = 0;
-
-			std::cout << std::endl << new_url_message << url << std::endl << std::endl;
-			}
-		}
-
-	ofile.close();
-	return 0;
-	}
+#endif /* __URLSEARCH_H__ */
